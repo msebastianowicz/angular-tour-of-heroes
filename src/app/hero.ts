a434f81ae489081e88c585b42546bf769484e8d5
Mateusz.S <mateuszsebastianowicz@gmail.com>
2022-08-26 10:24:10 +0200

    angular tour of heroes

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export interface Hero {
+    id: number;
+    name: string;
+    life_level: number;
+  }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', life_level: 99 },
+  { id: 12, name: 'Narco', life_level: 89},
+  { id: 13, name: 'Bombasto', life_level: 100},
+  { id: 14, name: 'Celeritas', life_level: 12},
+  { id: 15, name: 'Magneta', life_level: 80},
+  { id: 16, name: 'RubberMan', life_level: 110},
+  { id: 17, name: 'Dynama', life_level: 70},
+  { id: 18, name: 'Dr IQ', life_level: 100},
+  { id: 19, name: 'Magma', life_level: 13},
+  { id: 20, name: 'Tornado', life_level: 60}
+];
